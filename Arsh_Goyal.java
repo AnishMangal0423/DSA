1f65084fd02bca6adb4bbc1d1d90c6b0ee78f173
Anish Mangal <anishm0423@gmail.com>
2023-08-03 22:29:43 +0530

    Added q-2 in Arsh Folder

--- Arsh_Goyal.java
+++ Arsh_Goyal.java
@@ -1,118 +1,62 @@
-/*
-  Q1. You are given two 0-indexed arrays nums and cost consisting each of n positive integers.
-    You can do the following operation any number of times:
-    Increase or decrease any element of the array nums by 1.
-    The cost of doing one operation on the ith element is cost[i].
+/* 
+Q2. Construct Binary Search Tree from Preorder Traversal.
+    Input: preorder = [8,5,1,7,10,12]
+    Output: [8,5,10,1,7,null,12]
 
-    Return the minimum total cost such that all the elements of the array nums become equal.
+Soln- 
+    Intution- we have to make bst basically by using some nodes .. so it takes o(n^2) time , becuase we have to go in depth and add that node so adding n numbers will go for o(n^2) time.
 
-  Input: nums = [1,3,5,2], cost = [2,3,1,14]
-  Output: 8
+    Approch-1 : Brute Force-
+          TC- O(N^2)
+          SC- O(1)
 
+    Approch-2 : Approch of Range (Optimized)-
+        Intution- Always Remember whenever we are dealing with bst then range is important because it automatically maintains range and we can directly add using this range using low , high range ,, otherwise we have to tranverse the whole tree again-
+        
+        TC-O(N)
+        SC-O(1)
 
- Soln- 
-   Intution- Instead of making the number same in whole array to be equal another number it will be optimal to make same as number is present in array  as you can understand from above question , now to find the median in these weighted median question
-   
-   Method-1 : Brute Force
-   Intution- Instead of Finding median we are making target to every element and finding result to be minimum corresponding to it.
-             and taking the minimum-
 
     Code-
-         
-    public long minCost(int[] nums, int[] cost) {
-        int n1=nums.length;
-        int n2=cost.length;
-        long Minimum=Long.MAX_VALUE;     *** Mistake - I was taking it as INTEGER.MAX_VALUE
-         for(int i=0; i<n1; i++){
-              int target=nums[i];
-              long Mini=0;
-              for(int j=0; j<n1; j++){
-
-                  Mini+=Math.abs((long)nums[j]-(long)target)*(long)cost[j];
-              }
-            Minimum=Math.min(Mini , Minimum);
-            System.out.println(Minimum);
-         }
-     }         
-
-     TC- O(N2)
-     SC- O(1)
-
-   Method-2 : Method of Median-
-   Intution- As we have done above question we understand that if we take/Make  every elemnets costs to be equal to 1 then we can calculate the median and in O(N) time we will reach the result by sorting in O(NlogN) time.
-   Now we will change every elements costs as 1 by adding elements in num to be equal to cost so if they change to some number cost can be obtained , so we first took sum of all elements/costs and find median index by sc/2 , now , by prefix sum we calculated the range and median value and returned and calculated the difference/oprn in it.
-
-
-   lass Solution {
-   public long weightedMedian(long[][]sample , long pos){
-     int n1=sample.length;
-       Arrays.sort(sample , (a, b)->Long.compare(a[0],b[0]));
-    long store=0;
-       for(int i=0; i<n1; i++){
-        sample[i][1]+=store;
-        store=sample[i][1];
-        if(pos <= sample[i][1]){
-
-            return sample[i][0];
-        }
-
-       }
-
-       return 0;
-   }
-
-    public long minCost(int[] nums, int[] cost) {
-        int n1=nums.length;
-        int n2=cost.length;
-              long Mini=0;
-       
-   long[][]sample=new long[n1][2];  --> *** Mistake2 - It is Mandatory to make sample array to be long int cant ne used it will give wrong .
-
-   for(int i=0; i<n1; i++){
-       for(int j=0; j<2; j++){
-
-           if(j==0){
-               sample[i][j]=(long)nums[i];
-           }
-           else{
-               sample[i][j]=(long)cost[i];
-           }
-       }
-   }
-
-sum of costs-
-long sc=0;
-   for(int i=0; i<n1; i++){
-    sc+=(long)cost[i];
-   }
-
-   long pos=(sc+1)/2;   --> *** Mistake1 - if we will write sc then it not comapre to odd cases but can run for some cases because there range is large
-
-   long median=weightedMedian(sample , pos);
-
-
- for(int i=0; i<n1; i++){
-
-       Mini+=Math.abs((long)nums[i]-median)*(long)cost[i];  --> TypeCasting is Important
     
- }
-return Mini;
+    class Solution {
+
+int index=1;
+
+public void MakeTree(int pre[] ,int s_range , int l_range , TreeNode tree){
+    
+    if(index >= pre.length){
+        return;
+    }
+              
+        if(pre[index] < tree.val && pre[index] > s_range){
+           TreeNode node= new TreeNode(pre[index]);
+            tree.left=node;
+         index++;
+            
+            MakeTree(pre , s_range , tree.val , node);
+        }
+      
+        
+        if(index < pre.length && pre[index] > tree.val && pre[index] < l_range){
+                TreeNode node= new TreeNode(pre[index]);
+            // System.out.println("data"+node.val+" index "+index+" lvalue "+s_range);
+             tree.right=node;
+             index++;
+             MakeTree(pre , tree.val , l_range , node);
+        }
+        
+      
+    return;
+    
+}
+    public TreeNode bstFromPreorder(int[] pre) {
+        
+         TreeNode Tree=new TreeNode(pre[0]);
+    MakeTree(pre , Integer.MIN_VALUE , Integer.MAX_VALUE , Tree);
+      
+    return Tree;
     }
 }
-
-  TC- O(nLogN)
-  SC- O(N)
-
-
-```
-
-Note- 1. Whenever we have to convert All array elemnets to some value remember the concept of median.
-      2. If Cost is Given remember frequency median concept.
-      3. whenever even/odd elements are issue check for ind , indx+1 for errors.
-      4. Use Long.Max_Value if Long is in useCase.  
-
-```
-
-=============================================================================================================================================
-   
+    
  */
