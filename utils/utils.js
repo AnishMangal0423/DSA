9a0f2af07ab91edf2cd2f83b86b552b411986421
Anish Mangal <anishm0423@gmail.com>
2023-08-14 18:07:56 +0530

    FEAT : Added StringBuilder Property

---
+++ utils/utils.js
@@ -0,0 +1,12 @@
+/*
+
+1. StringBuilder --> StringBuilder has append function that can add any data type to it.. 
+    eg- StringBuilder s=new StringBuilder();
+        s.append('A');
+        s.append(true);
+
+        Ans- Atrue
+    
+
+*/
+ 
