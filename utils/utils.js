76df1a73bf192248cef623dfc29a390e087109c8
Anish Mangal <anishm0423@gmail.com>
2023-08-15 20:09:18 +0530

    Prop: Traversal of HashMap

--- utils/utils.js
+++ utils/utils.js
@@ -8,5 +8,32 @@
         Ans- Atrue
     
 
+  2.   Sorting of String -->
+                We cannot directly convert sorting of string by sort method .. we have to go via approch of stringBuilder or toCharArray approch-
+                
+                char[]carray=str.toCharArray()
+                 Arrays.sort(carray);
+                 String s=new String(carray)      
+
+
+  3.  Traversal of HashMap- 
+         we learnt traversal of Hashmap in 3 different ways-
+         a. for(Map.Entry<a, b>entry=map.entrySet()){
+
+            key=entry.getKey()
+            val=entry.getValue()
+         }            
+
+         b. // by key-
+            for(List<a>obj : map.keySet()){
+
+            }
+
+         c. // by value-
+         
+           for(List<a>val : map.values()){
+
+            
+           }              
 */
  
